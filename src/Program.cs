c37c94e11e0904e2ec8ab00c6937b34b6228d090
Maciek Rylko <rylek@MacBook-Pro-Maciek.local>
2016-02-12 22:14:51 +0100

    work in progress... add debug params

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,63 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Hackerrank
 {
+    internal static class Extensions
+    {
+        public static void AddOrInit<T>(this List<T> list, T item)
+        {
+            if (list == null)
+            {
+                list = new List<T> { item };
+            }
+            else
+            {
+                list.Add(item);
+            }
+        }
+        
+        public static IList<int> GetAt(this IList<int>[] matrix, int index) {
+            if (matrix[index] == null) {
+                matrix[index] = new List<int>();
+            }
+            
+            return matrix[index];
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.Read();
+            var line = Console.ReadLine().Split(' ');
+
+            var lastans = 0;
+            var n = int.Parse(line[0]);
+            var q = int.Parse(line[1]);
+            
+            var matrix = new List<int>[n];
+
+            for (var i = 0; i < q; i += 1)
+            {
+                line = Console.ReadLine().Split(' ');
+                var queryType = int.Parse(line[0]);    
+                var x = int.Parse(line[1]);    
+                var y = int.Parse(line[2]);  
+                
+                var index = ((x ^ lastans) % n);
+                if (queryType == 1) {
+                    matrix[index].AddOrInit(y);
+                } else {
+                    var valueIndex = matrix.GetAt(index).Count == 0 ? 0 : (y % matrix.GetAt(index).Count);
+                    var val = matrix[index][valueIndex];
+                    
+                    Console.WriteLine("Printing val {0}, from collection {1}", valueIndex, val);
+                    
+                    Console.WriteLine(val);
+                    lastans = val;
+                }
+            }
         }
     }
 }
